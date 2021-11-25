2ed6b44a7bae7985955d9ba9b1099c0fcdaf2077
efron licht <efron.dev@gmail.com>
2021-11-25 10:10:54 -0800

    prototype for Eval()

--- engine/engine.go
+++ engine/engine.go
@@ -1,1 +1,8 @@
 package engine
+
+type Board struct{} // TODO
+type Move struct{}  // TODO
+// Evaluate a board position.
+func Eval(b Board, m Move) (next Board, winning bool, err error) {
+	panic("not implemented")
+}
